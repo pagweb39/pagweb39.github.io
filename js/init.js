9b2c43558b760e4fc9ece3241a7534d456c3aaea
pagweb39 <pagweb39@gmail.com>
2021-06-03 19:19:40 -0500

    Cambios

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyB_FEL37xNzpsFFoncBpNjZ7naMEHxe3R4",
-    authDomain: "bdpagweb39.firebaseapp.com",
-    projectId: "bdpagweb39",
-    storageBucket: "bdpagweb39.appspot.com",
-    messagingSenderId: "1042352978271",
-    appId: "1:1042352978271:web:e8bb6e32a7bf0b85565eea",
-    measurementId: "G-4HK8T02BGE"
+  apiKey: "AIzaSyB_FEL37xNzpsFFoncBpNjZ7naMEHxe3R4",
+  authDomain: "bdpagweb39.firebaseapp.com",
+  projectId: "bdpagweb39",
+  storageBucket: "bdpagweb39.appspot.com",
+  messagingSenderId: "1042352978271",
+  appId: "1:1042352978271:web:e8bb6e32a7bf0b85565eea",
+  measurementId: "G-4HK8T02BGE"
 });
